da41d2615e5ba48871dc1ed6d325bbab4542098d
Giovanni Di Rosa <giodirosaartes@gmail.com>
2021-11-25 22:59:49 -0300

    se importa el modulo de path y se usa dirname para enturar html static

--- src/app.js
+++ src/app.js
@@ -3,14 +3,22 @@
 //cliente = navegador
 
 const express = require("express");
-const { route } = require("./routes/index.routes");
 const app = express(); // usar app como http
+const path = require("path"); // modulo que se encarga de normalizar rutas por temas de sistema de operativo
 
 //ROUTES
 // const routes = require("./routes/index.routes");
 // app.use(routes);
 // la versión shortcut
 app.use(require("./routes/index.routes"));
+
+// static files
+app.use(express.static(path.join(__dirname, "../public")));
+// console.log(__dirname);
+
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
 app.listen(3000, () => {
   console.log("servidor a la espera de conexiones");
